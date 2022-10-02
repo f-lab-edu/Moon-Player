1d07c6a5d2c891152c6fb37fba975533cc068b3f
TheGiantKiller <ktsdsd94@gmail.com>
2022-10-02 23:40:50 +0900

    feat : LoginPage를 MusicPage와 React-Router-dom 연동

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import LoginPage from "pages/Login/LoginPage";
+import MusicPage from "pages/Music/MusicPage";
 
 function App() {
-  return <LoginPage />;
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />}></Route>;
+      <Route path="/music" element={<MusicPage />}></Route>;
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/Music/MusicPage.jsx
@@ -0,0 +1,5 @@
+const MusicPage = () => {
+  return <div>Music Page</div>;
+};
+
+export default MusicPage;
