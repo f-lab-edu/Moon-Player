b88b4cf68ff778d49610853bbcae737f7e083d87
TheGiantKiller <ktsdsd94@gmail.com>
2022-10-02 23:40:50 +0900

    refactor: LoginMain 컴포넌트 정리

--- src/pages/Login/components/LoginMain.jsx
+++ src/pages/Login/components/LoginMain.jsx
@@ -1,7 +1,10 @@
 import styled from "styled-components";
 import Avatar from "components/items/Avatar";
+import { Link } from "react-router-dom";
+import LoginBox from "./LoginBox";
+import SignBox from "./SignBox";
 
-const MainContainer = styled.main`
+const Container = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -12,73 +15,22 @@ const MainContainer = styled.main`
   border-radius: 10px;
 `;
 
-const Container = styled.div`
+const Wrapper = styled.div`
   width: 100%;
   margin-bottom: 20px;
 `;
-const Button = styled.button`
-  width: 100%;
-  height: 60px;
-  font-size: 20px;
-  margin: 10px 0px;
-  background-color: white;
-  border: 1px solid rgba(0, 0, 0, 0.3);
-  background-color: #6633cc;
-  color: white;
-  border-radius: 6px;
-`;
-
-const Input = styled.input`
-  width: 500px;
-  height: 40px;
-  border: 0;
-  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
-  outline-width: 0;
-
-  padding: 10px;
-  margin-top: 20px;
-  font-size: 20px;
-`;
-
-const SignBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  > a {
-    display: flex;
-    justify-content: flex-end;
-    margin-top: 10px;
-    text-decoration: none;
-  }
-`;
-
-const RegisterBox = styled.div`
-  > div {
-    display: flex;
-    gap: 5px;
-  }
-`;
-
 
 const LoginMain = () => {
   return (
-    <MainContainer>
+    <Container>
       <Avatar />
-      <Container>
-        <SignBox>
-          <Input placeholder="Email" type="email" />
-          <Input placeholder="Password" type="password" />
-          <a href="#">아이디 / 비밀번호 찾기</a>
-        </SignBox>
-      </Container>
-      <Container>
-        <RegisterBox>
-          <div>
-            <Button>로그인</Button>
-            <Button>회원가입</Button>
-          </div>
-        </RegisterBox>
-      </Container>
-    </MainContainer>
+      <Wrapper>
+        <SignBox />
+      </Wrapper>
+      <Wrapper>
+        <LoginBox />
+      </Wrapper>
+    </Container>
   );
 };
 
