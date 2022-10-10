036dbd2efb991626919a5bc4d568b1f02ba18001
TheGiantKiller <ktsdsd94@gmail.com>
2022-10-11 00:20:00 +0900

    feat : Music Page 반응형 웹 UI 구현 완료

--- src/pages/Music/components/NavItems.jsx
+++ src/pages/Music/components/NavItems.jsx
@@ -5,15 +5,35 @@ import Card from '../../../components/items/Card';
 import Layout from 'components/items/Layout';
 
 const NavLayout = styled(Layout)`
+  
+    display: flex;
     flex-direction: row;
     justify-content: center;
     align-items: center;
+
+
+    @media screen and (max-width:1000px){
+    flex-direction: column;
+    width: 100%;
+    gap:10px 
+    }
+
+    
+    
+  
 `
 
 const NavCard = styled(Card)`
 box-shadow: 2px 1px 10px 1px rgba(133, 99, 99, 0.3);
 color: #6633cc;
 font-weight :900;
+@media screen and (max-width:1000px){
+  border-radius: 7px;
+  font-size: 25px;
+  font-weight:bold;
+  width:90%;
+  box-shadow: 2px 1px 10px 1px rgba(0, 0, 0, 0.2);
+}
 `
 
 const NavItems = () => {
@@ -26,7 +46,8 @@ const NavItems = () => {
       <NavCard>해외 랩/힙합</NavCard>
       <NavCard>해외 랩/힙합</NavCard>
       <FontAwesomeIcon icon={faCircleArrowRight} size={'2x'} color={'#6633cc'} />
-    </NavLayout>
+
+    </NavLayout >
   )
 }
 export default NavItems
