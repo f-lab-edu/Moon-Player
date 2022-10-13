49955b4092bde12ecf32159fe2334596aed7f434
TheGiantKiller <ktsdsd94@gmail.com>
2022-10-13 18:10:10 +0900

    feat : 클라이언트와 서버 부분 concurrently하게 동작하기 setting

--- mock-server/src/index.js
+++ mock-server/src/index.js
@@ -1,13 +1,15 @@
 const Koa = require('koa');
 const Router = require('koa-router')
 const router = new Router()
-const app = new Koa();
 const api = require('./api')
 
 router.use('/api', api.routes())
 
+const app = new Koa();
 app.use(router.routes()).use(router.allowedMethods())
 
-app.listen(4000, () => {
+
+const PORT = 4000
+app.listen(PORT, () => {
   console.log('server is listening to port 4000');
 });
