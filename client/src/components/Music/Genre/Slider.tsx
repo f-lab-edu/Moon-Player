670de27e779b78be3d94bdfbf24d06450ad2252e
TheGiantKiller <ktsdsd94@gmail.com>
2022-11-27 20:40:32 +0900

    fix : Genre Slider 컴포넌트 슬라이드 개선

--- client/src/components/Music/Genre/Slider.tsx
+++ client/src/components/Music/Genre/Slider.tsx
@@ -10,29 +10,20 @@ interface SliderProps {
 }
 
 export const Slider = ({ children }: SliderProps) => {
-  const { setSwiperModule, swiperState, handleSlideChange, sliderState, handleNextSlide, handlePrevSlide } =
-    useSlider();
+  const { setSwiperModule, swiperState, handleNextSlide, handlePrevSlide } = useSlider();
   const genreCards = children.map(({ genre_img, genre_id }) => (
     <SwiperSlide key={genre_id}>
       <Item genre_img={genre_img} key={genre_id} genre_id={genre_id}></Item>
     </SwiperSlide>
   ));
-
   return (
     <>
-      {!sliderState.isBegining ? (
-        <StyledIconButton icon="arrowLeft" size="3x" color="white" onClick={handlePrevSlide}></StyledIconButton>
-      ) : (
-        <></>
-      )}
-      <Swiper {...swiperState} onSlideChange={handleSlideChange} onSwiper={(swiper) => setSwiperModule(swiper)}>
+      <StyledIconButton icon="arrowLeft" size="3x" color="white" onClick={handlePrevSlide}></StyledIconButton>
+
+      <Swiper {...swiperState} onSwiper={(swiper) => setSwiperModule(swiper)}>
         {genreCards}
       </Swiper>
-      {!sliderState.isEnd ? (
-        <StyledIconButton icon="arrowRight" size="3x" color="white" onClick={handleNextSlide}></StyledIconButton>
-      ) : (
-        <></>
-      )}
+      <StyledIconButton icon="arrowRight" size="3x" color="white" onClick={handleNextSlide}></StyledIconButton>
     </>
   );
 };
--- client/src/hooks/useSlider.tsx
+++ client/src/hooks/useSlider.tsx
@@ -6,36 +6,22 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { useEffect, useState } from 'react';
 import SwiperCore from 'swiper';
-//sliderstate는 swiper Module에 의존적인 부분이 많아서 따로 분리를 시킬필요가없을거같다
+//slider state는 swiper Module에 의존적인 부분이 많아서 따로 분리를 시킬필요가없을거같다
 export const useSlider = () => {
   const [swiperModule, setSwiperModule] = useState<SwiperCore>();
   const [swiperState, setSwiperState] = useState<SwiperProps>(DEFAULT_SETTING);
 
-  //  sliderState는 slider를 관리
-  const [sliderState, setSliderState] = useState(DEFAULT_SLIDER_SETTING);
-
   useEffect(() => {
     // swiper 모듈이 없으면 실행불가
     if (!swiperModule) return;
-    setSliderState({ ...sliderState, isBegining: true, isEnd: false });
   }, [swiperModule]);
-  const handleSlideChange = () => {
-    if (!swiperModule) return;
-    setSliderState({ ...sliderState, isBegining: swiperModule.isBeginning, isEnd: swiperModule.isEnd });
-  };
 
   // 스와이퍼 모듈로 넘길수있음
-  const handleNextSlide = () => swiperModule?.slideNext();
-  const handlePrevSlide = () => swiperModule?.slidePrev();
-  return { sliderState, setSwiperModule, swiperState, handleNextSlide, handlePrevSlide, handleSlideChange };
+  const handleNextSlide = () => swiperModule?.slideNext(1000);
+  const handlePrevSlide = () => swiperModule?.slidePrev(1000);
+  return { setSwiperModule, swiperState, handleNextSlide, handlePrevSlide };
 };
 export default useSlider;
-
-const DEFAULT_SLIDER_SETTING = {
-  isBegining: false,
-  isEnd: false,
-};
-
 // 스와이퍼 관련된 라이브러리 셋팅 만들어올수있음
 const DEFAULT_SETTING: SwiperProps = {
   modules: [Pagination],
@@ -46,21 +32,19 @@ const DEFAULT_SETTING: SwiperProps = {
   slidesPerView: 6,
   slidesPerGroup: 6,
   allowTouchMove: false,
-
+  loopPreventsSlide: false,
   breakpoints: {
     480: {
       slidesPerView: 2,
       slidesPerGroup: 2,
       spaceBetween: 30,
     },
-    // when window width is >= 630px
     768: {
       slidesPerView: 3,
       slidesPerGroup: 3,
 
       spaceBetween: 15,
     },
-    // when window width is >= 1000
     1024: {
       slidesPerView: 6,
       slidesPerGroup: 6,
