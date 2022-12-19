b0c37e95736294394a14b08a1360653556aedd59
TheGiantKiller <ktsdsd94@gmail.com>
2022-12-19 21:15:00 +0900

    feat : react-testing-library를 위한 redux-toolkit 테스트 유틸 렌더함수 생성

---
+++ client/src/test-utils/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render as rtlRender } from '@testing-library/react';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import { rootReducer } from '../store/reducers/index';
+
+function render(
+  ui,
+  {
+    preloadedState,
+    store = configureStore({
+      reducer: rootReducer,
+    }),
+    ...renderOptions
+  } = {}
+) {
+  function Wrapper({ children }) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+}
+
+export * from '@testing-library/react';
+export { render };
