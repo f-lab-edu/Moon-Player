1b5f6625d0e6bad840e0e5965b549a879b55a4b8
TheGiantKiller <ktsdsd94@gmail.com>
2022-12-20 22:07:25 +0900

    chore : 불필요한 설정 제거 및 수정

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,3 +1,4 @@
+// jest에서 읽기위해 트랜스컴파일 해줘야함
 module.exports = {
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }],
--- client/jest.config.js
+++ client/jest.config.js
@@ -2,6 +2,8 @@ module.exports = {
   testEnvironment: 'jsdom', // jsdom으로 수정
   testPathIgnorePatterns: [ '<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/setupTest.ts'], // setupTest를 이용하여 환경 설정
+
+  // import 모듈 못읽어서 babel-jest사용
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest',
     '^.+\\.(js|jsx)?$': 'babel-jest'
