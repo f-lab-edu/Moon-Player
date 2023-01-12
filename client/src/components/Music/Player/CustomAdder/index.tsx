12463dea22e7cfc1d0496ec7b41e073531509366
TheGiantKiller <ktsdsd94@gmail.com>
2023-01-12 22:53:22 +0900

    feat : CustomAdder 컴포넌트에 현재 재생목록 추가

--- client/src/components/Music/Player/CustomAdder/index.tsx
+++ client/src/components/Music/Player/CustomAdder/index.tsx
@@ -4,10 +4,22 @@ import IconButton from 'components/Global/UI/IconButton/IconButton';
 import Flex from 'components/Global/style/Flex';
 import Button from 'components/Global/style/Button/Button';
 import { Text } from 'components/Global/style/Text';
+import { useAppSelector } from 'hooks/useAppDispatch';
+import { MusicItem } from 'components/Global/UI/MusicItem/MusicItem';
+import { ScrollBox } from '../../../Global/style/ScrollBox';
 
 export const CustomAdder = () => {
+  const playerSelector = useAppSelector((state) => state.music.player);
+  const playerMusics =
+    playerSelector.list.length > 0 ? (
+      playerSelector.list.map(({ name, img_url }, index) => (
+        <MusicItem name={name} img_url={img_url} key={index} id={++index} />
+      ))
+    ) : (
+      <EmptyText>재생 목록이 비어있습니다.</EmptyText>
+    );
   return (
-    <Layout direction="column" justifyContent="center" alignItems="center">
+    <Layout direction="column" justifyContent="center">
       <Flex direction="row" justifyContent="space-between" alignItems="center">
         <Flex direction="row" alignItems="center" gap="15px">
           <InputTitle>이름</InputTitle>
@@ -15,10 +27,16 @@ export const CustomAdder = () => {
         </Flex>
         <SaveButton>저장</SaveButton>
       </Flex>
+      <MusicBox>{playerMusics}</MusicBox>
     </Layout>
   );
 };
 export default CustomAdder;
+
+const MusicBox = styled(ScrollBox)`
+  height: 30vh;
+  padding-right: 10px;
+`;
 const Layout = styled(Flex)``;
 const SaveButton = styled(Button)`
   color: black;
@@ -31,14 +49,18 @@ const SaveButton = styled(Button)`
   margin: 10px;
 `;
 const InputBox = styled.input`
-  width: 230px;
+  width: 300px;
   font-size: 18px;
   outline: none;
   border: none;
   border-radius: 7px;
   padding: 5px;
 `;
+const EmptyText = styled(Text)`
+  color: rgba(255, 255, 255, 0.64);
 
+  font-size: 20px;
+`;
 const InputTitle = styled(Text)`
   font-size: 17px;
   width: 40px;
